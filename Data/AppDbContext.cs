33673339678f81e784fb5610fc860bb928bf8c87
Miroslav Bartos <mirek-b@seznam.cz>
2020-07-28 11:58:44 -0700

    Setup DB context for localdb

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using siemens.Models;
+
+namespace siemens.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<siemens.Models.Machine> Machine { get; set; }
+    }
+}
