a797504c54739e6d84aeb31c5ec6598580c12f9a
Miroslav Bartos <mirek-b@seznam.cz>
2020-07-28 11:57:16 -0700

    Create Machine model class

---
+++ Models/Machine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace siemens.Models
+{
+    public class Machine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ReleaseYear { get; set; }
+        public int MaxTemperature { get; set; }
+        public int MinVoltage { get; set; }
+    }
+}
